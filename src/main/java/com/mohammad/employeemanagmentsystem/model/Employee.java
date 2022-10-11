41873a5193e335a5961e9e473bc4cc2388ebc554
Mohammad1374-kh <mohammad1374.kh@gmail.com>
2022-10-12 00:25:44 +0330

    Creating model package & adding employee class

---
+++ src/main/java/com/mohammad/employeemanagmentsystem/model/Employee.java
@@ -0,0 +1,10 @@
+package com.mohammad.employeemanagmentsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/*@Entity
+@Table(name = "employees")*/
+public class Employee {
+
+}
