b0ba7667aa960d49cef16e08372a824c7303f16d
Mohammad1374-kh <mohammad1374.kh@gmail.com>
2022-10-11 20:06:55 +0330

    Empty EMS project

---
+++ src/test/java/com/mohammad/employeemanagmentsystem/EmployeeManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammad.employeemanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
