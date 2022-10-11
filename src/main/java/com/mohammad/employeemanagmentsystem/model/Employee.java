eee3622843db679ea3e120588c3526daed1b44ec
Mohammad1374-kh <mohammad1374.kh@gmail.com>
2022-10-12 01:59:48 +0330

    completing Employee model

--- src/main/java/com/mohammad/employeemanagmentsystem/model/Employee.java
+++ src/main/java/com/mohammad/employeemanagmentsystem/model/Employee.java
@@ -1,10 +1,30 @@
 package com.mohammad.employeemanagmentsystem.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-/*@Entity
-@Table(name = "employees")*/
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "employee_code")
+    private String employeeCode;
+
 
 }
