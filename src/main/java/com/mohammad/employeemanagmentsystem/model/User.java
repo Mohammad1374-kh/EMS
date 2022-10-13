ab3bb3739b03a115c27d30d66e7d98632c6a86b9
Mohammad1374-kh <mohammad1374.kh@gmail.com>
2022-10-13 11:49:15 +0330

    completing user model

--- src/main/java/com/mohammad/employeemanagmentsystem/model/User.java
+++ src/main/java/com/mohammad/employeemanagmentsystem/model/User.java
@@ -7,23 +7,21 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "employees")
+@Table(name =  "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Employee {
-
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email")
+
     private String email;
-    @Column(name = "employee_code")
-    private String employeeCode;
+    private String password;
 
 
 }
